c73b0ee2c330d299496f69012fc4c0e73591ea91
PletosuRazvan <100827082+PletosuRazvan@users.noreply.github.com>
2023-03-18 13:21:21 +0200

    Update credential.py

--- models/credential.py
+++ models/credential.py
@@ -1,12 +1,18 @@
 from enum import Enum
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, DateTimeField, EmailField, BooleanField, IntField
 
 class RoleType(Enum):
-    ADMIN  = "ADMIN"
-
+    ADMIN = "ADMIN"
+    PATIENT = "PATIENT"
+    DOCTOR = "DOCTOR"
+    NURSE = "NURSE"
 
 class Credential(Document):
-    email = StringField(required=True, unique=True)
+    id = IntField(primary_key=True)
+    email = EmailField(required=True, unique=True)
     password = StringField(required=True)
-
-    
+    fullName = StringField(required=True)
+    dateOfBirth = DateTimeField(required=False, trim=True)
+    phoneNumber = StringField(required=True, trim=True) 
+    role = StringField(required=True, choices=[role.value for role in RoleType], default=RoleType.PATIENT.value)
+    isConfirmed = BooleanField(required=True, default=False)
