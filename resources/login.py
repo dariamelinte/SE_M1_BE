588ad938ce1c76176a4ba43507e239ae99ca1e89
PletosuRazvan <100827082+PletosuRazvan@users.noreply.github.com>
2023-03-23 23:51:50 +0200

    Update login.py

--- resources/login.py
+++ resources/login.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_cors import CORS
+import hashlib
+from models.credentials import Credentials
 
 
 # /login:id
@@ -8,9 +10,20 @@ class Login(Resource):
     def post(self):
         data = request.get_json() if request.get_json() else {}
         email = data.get('email')
-        password = data.get('password')
+        password = data.pop('password')
 
-        print(password)
+        password_bytes = password.encode('utf-8')
+        hash_256 = hashlib.sha256()
+        hash_256.update(password_bytes)
+        data['password'] = hash_256.hexdigest()
+
+
+        try:
+            credential = Credentials.objects.all
+            print (credential)
+            
+        except Exception as e : 
+            return False
 
         return {
             "success": True,
