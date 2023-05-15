2c46cfa45b8f788c1272df1256188f7d1e568871
daria melinte <dariamelinte2003@gmail.com>
2023-05-15 14:07:44 +0300

    Add logout route

--- resources/logout.py
+++ resources/logout.py
@@ -3,16 +3,42 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
 import jwt
 
+from models.users import Users
+
 class Logout(Resource):
     def post(self):
-        print("logout")
-        # token = request.headers.get('Authorization')
-        # if not token:
-        #     return {"success": False, "message": "No token provided."}, 401
+        data = request.get_json() if request.get_json() else {}
+        try:
+            # Decode the JWT token
+            payload = jwt.decode(data.get("jwt"), "m1-personal-jwt", algorithms=["HS256"])
+    
+            user = Users.objects(id=payload["user_id"]).first()
 
-        # try:
-        #     return {"success": True, "message": "Logged out successfully."}, 200
-        # except jwt.ExpiredSignatureError:
-        #     return {"success": False, "message": "Signature expired. Please log in again."}, 401
-        # except jwt.InvalidTokenError:
-        #     return {"success": False, "message": "Invalid token. Please log in again."}, 401
+            if user is None:
+                return {
+                    "success": False,
+                    "message": "Invalid token",
+                }
+            
+            user.jwt = None
+            user.save()
+            
+            return {
+                "success": True,
+                "message": "Logout successful"
+            }
+        except jwt.ExpiredSignatureError:
+            return {
+                "success": False,
+                "message": "Token has expired"
+            }, 500
+        except jwt.InvalidTokenError:
+            return {
+                "success": False,
+                "message": "Invalid token"
+            }, 500
+        except Exception as e:
+            return {
+                "success": False,
+                "message": "Oops, something went wrong, please try again."
+            }, 500
